b64ac39c1c7bbc718b2eb8cd8e6356de836383ab
sheraz10 <52635053+sheraz10@users.noreply.github.com>
2019-07-29 01:30:27 -0700

    Add files via upload

---
+++ question1sol.py
@@ -0,0 +1,26 @@
+
+inp = input("please enter an integer input: ")
+inp=(int)(inp)
+sec=inp
+total=inp
+
+for var in range(2):
+    sec=sec*inp
+    total=total+sec
+
+print("n+nn+nnn ="+ str(total))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
